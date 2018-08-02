387d5347bf3ec03a783b1d4b97b31ab360e6d55e
summer <wpsumsun@126.com>
2018-08-03 01:20:00 +0800

    将 svg 修改为icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'icon',
+  props: ['name']
+}
+</script>
+
+<style lang="scss" scoped>
+  .g-icon {
+    width: 1em;
+    height: 1em;
+  }
+</style>
