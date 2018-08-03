9d85f75ae2965fac01cc898f845560b7533237d2
wpsumsun <wpsumsun@126.com>
2018-08-03 19:13:57 +0800

    button-group 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import buttonGroup from './button-group'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', buttonGroup)
 
 new Vue({
   el: '#app',
