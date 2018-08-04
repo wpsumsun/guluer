5b11d53c23c8d5aca944c9de1336682d3c121ab4
summer <wpsumsun@126.com>
2018-08-05 01:01:36 +0800

    接入 travis

--- src/button.vue
+++ src/button.vue
@@ -2,7 +2,7 @@
   <button
     class="g-button"
     :class="{ [`icon-${iconPosition}`]: !!iconPosition }"
-    @click="$emit('clisck')">
+    @click="$emit('click')">
     <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
