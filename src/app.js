15e5aee34c04b7cdb36e317e1de206d11b422cdf
summer <wpsumsun@126.com>
2018-08-06 00:58:57 +0800

    input.vue 通过 input.test.js 测试

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import buttonGroup from './button-group'
+import Input from './input.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', buttonGroup)
+Vue.component('g-input', Input)
 
 new Vue({
   el: '#app',
---
+++ src/input.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="g-input">
+  <input 
+    type="text" 
+    :value="value" 
+    :disabled="disabled" 
+    :readonly="readonly"
+    @change="changeHandler"
+    @input="inputHandler"
+    @focus="focusHandler"
+    @blur="blurHandler">
+
+  <div v-if="error" class="errorMessage"><Icon name="error"></Icon>{{ error }}</div>
+</div>
+</template>
+
+<script>
+  import Icon from './icon.vue'
+  export default {
+    name: 'g-input',
+    components: {
+      Icon
+    },
+    props: {
+      value: {
+        type: [String, Number],
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      readonly: {
+        type: Boolean,
+        default: false,
+      },
+      error: {
+        type: String
+      }
+    },
+    methods: {
+      changeHandler(e) {
+        this.$emit('change', 'hi')
+      },
+      inputHandler(e) {
+        this.$emit('input', 'hi')
+      },
+      focusHandler(e) {
+        this.$emit('focus',  'hi')
+      },
+      blurHandler(e) {
+        this.$emit('blur',  'hi')
+      },
+    },
+  }
+</script>
