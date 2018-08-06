81fefa0ac52f581512a3b2f619990dc87661da7a
wpsumsun <wpsumsun@126.com>
2018-08-06 18:45:28 +0800

    添加 input.vue

--- src/input.vue
+++ src/input.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="g-input">
+<div class="wrapper" :class="{ error: error, tips: tips }">
   <input 
     type="text" 
     :value="value" 
@@ -10,7 +10,14 @@
     @focus="focusHandler"
     @blur="blurHandler">
 
-  <div v-if="error" class="errorMessage"><Icon name="error"></Icon>{{ error }}</div>
+  <template v-if="error">
+    <Icon name="warning" class="icon-warning"></Icon>
+    <span class="errorMessage">{{ error }}</span>
+  </template>
+  <template v-if="tips">
+    <Icon name="info" class="icon-info"></Icon>
+    <span class="tipsMessage">{{ tips }}</span>
+  </template>
 </div>
 </template>
 
@@ -35,6 +42,9 @@
       },
       error: {
         type: String
+      },
+      tips: {
+        type: String
       }
     },
     methods: {
@@ -53,3 +63,66 @@
     },
   }
 </script>
+
+<style lang="scss" scoped>
+  $height: 32px;
+  $border-color: #999;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $font-size: 12px;
+  $box-shadow-color: rgba(0, 0, 0, 0.5);
+  $red: #f56c6c;
+  $tipsColor: #67c23a;
+  .wrapper {
+    display: inline-flex;
+    align-items: center;
+    > *:not(:last-child) {
+      margin-right: .4em;
+    }
+    > input {
+      height: $height;
+      padding: 0 8px;
+      border: 1px solid $border-color;
+      border-radius: $border-radius;
+      font-size: inherit;
+      &:hover {
+        border-color: $border-color-hover;
+      }
+      &:focus {
+        outline: none;
+        box-shadow: inset 0 1px 3px $box-shadow-color;
+      }
+      &[disabled], &[readonly] {
+        border-color: #bbb;
+        color: #bbb;
+        cursor: not-allowed;
+      }
+    }
+    &.error {
+      input {
+        border-color: $red;
+        &:hover {
+          border-color: $red;
+        }
+        &:focus {
+          outline: none;
+          box-shadow: inset 0 1px 3px $red;
+        }
+      }
+      .icon-warning {
+        fill: $red;
+      }
+      .errorMessage {
+        color: $red;
+      }
+    }
+    &.tips {
+      .icon-info {
+        fill: $tipsColor;
+      }
+      .tipsMessage {
+        color: $tipsColor;
+      }
+    }
+  }
+</style>
