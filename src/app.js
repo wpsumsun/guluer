f18f036e3b68c3592d63a64d8a90b0476f8305d4
summer <wpsumsun@126.com>
2018-08-12 21:56:18 +0800

    layout布局基本完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import buttonGroup from './button-group'
 import Input from './input.vue'
 import Row from './row.vue'
 import Col from './col.vue'
+import Layout from './layout.vue'
+import Header from './header.vue'
+import Content from './content.vue'
+import Footer from './footer.vue'
+import Sider from './sider.vue'
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -12,6 +18,11 @@ Vue.component('g-button-group', buttonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
 
 new Vue({
   el: '#app',
---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'g-sider'
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
