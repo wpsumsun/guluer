7e29e2ce8508ef3e1063fb3e331db0964c52a2ab
summer <wpsumsun@126.com>
2018-08-16 00:12:10 +0800

    修复toast.test.js报错

--- test/toast.test.js
+++ test/toast.test.js
@@ -27,7 +27,7 @@ describe('Toast', () => {
       })
     })
 
-    it('接收 closeButton', () => {
+    it('接收 closeButton', (done) => {
       const Constructor = Vue.extend(Toast)
       const callback = sinon.fake()
       const vm = new Constructor({
@@ -40,8 +40,11 @@ describe('Toast', () => {
       }).$mount()
       const closeButton = vm.$el.querySelector('.close')
       expect(closeButton.innerHTML.trim()).to.equal('啊哈')
-      closeButton.click()
-      expect(callback).to.have.been.called
+      setTimeout(() => {
+        closeButton.click()
+        expect(callback).to.have.been.called
+        done()
+      }, 200)
     })
 
     it('接收 enableHtml', () => {
