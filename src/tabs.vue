be9308783cf2b99a09874a33987db56efc40dff8
summer <wpsumsun@126.com>
2018-08-19 23:51:19 +0800

    优化 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -32,19 +32,27 @@
 	    }
 		},
 		mounted() {
-	    if (!this.$children.length) {
-        console&&console.warn&&console.warn('Tbas组件的子组件不能为空')
-	    }
-      this.$children.forEach(vm => {
-        if (vm.$options.name === 'g-tabs-head') {
-          vm.$children.forEach(childVm => {
-            if (childVm.$options.name === 'g-tabs-item' && childVm.name === this.selected) {
-	            this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
-        }
-      })
-		}
+	    this.checkChildren()
+			this.selectTab()
+		},
+		methods: {
+	    checkChildren() {
+		    if (!this.$children.length) {
+	        console&&console.warn&&console.warn('Tbas组件的子组件不能为空')
+		    }
+	    },
+			selectTab() {
+	      this.$children.forEach(vm => {
+	        if (vm.$options.name === 'g-tabs-head') {
+	          vm.$children.forEach(childVm => {
+	            if (childVm.$options.name === 'g-tabs-item' && childVm.name === this.selected) {
+		            this.eventBus.$emit('update:selected', this.selected, childVm)
+	            }
+	          })
+	        }
+	      })
+			},
+		},
 	}
 </script>
 
