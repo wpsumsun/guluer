7e88a97ddf1343e6d8fbe7a35bbd698926f551b4
summer <wpsumsun@126.com>
2018-08-19 16:49:14 +0800

    创建popover组件

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ import TabsHead from './tabs-head'
 import TabsItem from './tabs-item'
 import TabsBody from './tabs-body'
 import TabsPane from './tabs-pane'
+import Popover from './popover'
 
 Vue.use(toast)
 
@@ -37,6 +38,7 @@ Vue.component('g-tabs-head', TabsHead)
 Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-body', TabsBody)
 Vue.component('g-tabs-pane', TabsPane)
+Vue.component('g-popover', Popover)
 
 new Vue({
   el: '#app',
---
+++ src/popover.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="popover" @click="visible = !visible">
+		<div class="content-wrapper" v-if="visible">
+			<slot name="content"></slot>
+		</div>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'g-popover',
+	  data() {
+      return {
+        visible: false
+      }
+	  },
+  }
+</script>
+
+<style lang="scss" scoped>
+	.popover {
+		display: inline-flex;
+		position: relative;
+		.content-wrapper {
+			width: 200px;
+			position: absolute;
+			bottom: 100%;
+			left: 0;
+			padding: 18px 20px;
+			border: 1px solid #ebeef5;
+			border-radius: 4px;
+			box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+		}
+	}
+</style>
