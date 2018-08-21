f5631c603ec4dbb5ed7ff5a44ba765b01edb9718
summer <wpsumsun@126.com>
2018-08-22 01:10:24 +0800

    将 parcel 迁移到 vue-cli

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<div class="box">
+			<g-collapse :selected.sync="currentTab">
+				<g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
+				<g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
+				<g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
+			</g-collapse>
+			{{ currentTab }}
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'app',
+		data() {
+	    return {
+        currentTab: ['2', '3']
+	    }
+		},
+	}
+</script>
