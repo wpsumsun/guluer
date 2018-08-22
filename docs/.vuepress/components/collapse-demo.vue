163a4583dacef5dc3821ed5c64cb1c113ac291a6
summer <wpsumsun@126.com>
2018-08-23 01:20:27 +0800

    修复官网collapse报错

--- docs/.vuepress/components/collapse-demo.vue
+++ docs/.vuepress/components/collapse-demo.vue
@@ -9,7 +9,7 @@
 			<pre><code>{{ code1 }}</code></pre>
 		</div>
 		<div class="box">
-			<g-collapse :selected.sync="currentTab1" single>
+			<g-collapse :selected.sync="currentTab" single>
 				<g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
 				<g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
 				<g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
