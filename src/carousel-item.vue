7c63163d40157d009b34a606170bc427ca167b3b
summer <wpsumsun@126.com>
2018-08-30 01:14:18 +0800

    carousel组件

---
+++ src/carousel-item.vue
@@ -0,0 +1,60 @@
+<template>
+	<transition name="slide">
+		<div class="g-carousel-item" v-show="visible" :class="{ reverse }">
+			<slot></slot>
+		</div>
+	</transition>
+</template>
+
+<script>
+  export default {
+    name: 'g-carousel',
+	  props: {
+      name: {
+        type: String,
+	      require: true
+      }
+	  },
+	  data() {
+      return {
+        selected: null,
+	      reverse: false
+      }
+	  },
+	  computed: {
+      visible() {
+        return this.selected === this.name
+      }
+	  },
+  }
+</script>
+
+<style lang="scss" scoped>
+.slide-enter-active,
+.slide-leave-active {
+	transition: all .4s;
+}
+.slide-enter.reverse {
+	transform: translateX(-100%);
+}
+.slide-leave-to.reverse {
+	transform: translateX(100%);
+	position: absolute;
+	left: 0;
+	top: 0;
+}
+.slide-enter {
+	transform: translateX(100%);
+}
+.slide-leave-to {
+	transform: translateX(-100%);
+	position: absolute;
+	left: 0;
+	top: 0;
+}
+.g-carousel-item {
+	width: 100%;
+	height: 100%;
+	flex-shrink: 0;
+}
+</style>
---
+++ src/carousel.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="g-carousel">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'g-carousel',
+	props: {
+    selected: {
+      type: String
+    },
+		autoPlay: {
+      type: Boolean,
+			default: true
+		},
+		duration: {
+      type: [Number, String],
+			default: 3
+		},
+		reverse: {
+      type: Boolean,
+			default: false
+		}
+	},
+	mounted() {
+    const names = this.$children.map(vm => vm.name)
+    let index = names.indexOf(this.getSelected())
+    const run = () => {
+	    if (index === names.length) { index = 0 }
+	    if (index === -1) { index = names.length - 1 }
+      this.$emit('update:selected', names[index])
+	    if (this.reverse) {
+        index --
+	    } else {
+        index ++
+	    }
+	    setTimeout(run, this.duration * 1000)
+    }
+    this.updateChildren()
+		setTimeout(run, this.duration * 1000)
+  },
+  updated() {
+    this.updateChildren()
+  },
+	methods: {
+    updateChildren() {
+      const selected = this.getSelected()
+	    this.$children.forEach(vm => {
+		    // const names = this.$children.map(vm => vm.name)
+		    // const current = names.indexOf(this.getSelected())
+		    // const index = names.indexOf(vm.name)
+        vm.reverse = this.reverse
+        vm.selected = selected
+	    })
+    },
+    getSelected() {
+      return this.selected || this.$children[0].name
+    },
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.g-carousel {
+	width: 450px;
+	height: 150px;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	overflow: hidden;
+	position: relative;
+	border: 1px solid #67c23a;
+}
+</style>
