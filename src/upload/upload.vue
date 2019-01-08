6e1cc57ad1a164b774456c744344cb1198421552
wpsumsun <wpsumsun@126.com>
2019-01-09 00:26:31 +0800

    target:添加进度条 status 待思考

--- src/upload/upload.vue
+++ src/upload/upload.vue
@@ -9,8 +9,10 @@
       <li v-for="(file, fileIndex) in fileList" :key="fileIndex">
         <img width="100px" :src="file.url">{{ file.name }}
         <g-icon name="close" @click="onRemoveFile(file)"></g-icon>  
+        <g-icon name="loading" v-if="file.isUploading"></g-icon>
       </li>
     </ul>
+    <div ref="prograss"></div>
   </div>
 </template>
 
@@ -47,6 +49,9 @@
         }
       }
     },
+    data() {
+      tempIndex: 1
+    },
     methods: {
       onRemoveFile(file) {
         let anwser = window.confirm('确定要删除吗')
@@ -65,13 +70,25 @@
           file && this.updateFile(file)
         })
       },
+      BeforeUpload(file) {
+        const { name, size } = file
+        this.$emit('update:fileList', [...this.fileList, { name, isUploading: true, size }])
+      },
       updateFile(file) {
+        console.log(file)
+        this.BeforeUpload(file)
         const { name, size } = file
         this.$refs.upload.value = null
         const formData = new FormData()
         formData.append(this.name, file)
         const xhr = new XMLHttpRequest()
         xhr.open(this.method, this.action)
+        xhr.upload.onprogress = function progress(e) {
+          if (e.total > 0) {
+            e.percent = e.loaded / e.total * 100;
+            console.log(e.percent)
+          }
+        };
         xhr.onload = () => {
           if(xhr.status == 200){
             const url = this.parseResponse(xhr.response)
